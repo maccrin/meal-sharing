a03e7ba2927518b21bac1b6bc76bb19ffcf67e15
maccrin <maccrin@gmail.com>
2023-01-30 18:13:19 +0100

    app.js file

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path");
-
+const knex = require("../backend/database");
 const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
@@ -20,12 +20,35 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+app.get('/my-route', (req, res) => {
+  res.send("Hi friend");
+});
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
   throw "API_PATH is not set. Remember to set it in your .env file"
 }
+app.get("/future-meals", async (req, res) => {
+  const rows = await knex.raw("SELECT * FROM `meal` WHERE `when` > NOW()");
+  rows[0].length === 0 ? res.sendStatus(404) : res.send(rows.slice(0, 1))
+});
+app.get("/past-meals", async (req, res) => {
+  const rows = await knex.raw("SELECT * FROM `meal` WHERE `when` <= NOW()");
+  rows[0].length === 0 ? res.sendStatus(404) : res.send(rows.slice(0, 1))
+});
+app.get("/all-meals", async (req, res) => {
+  const rows = await knex.raw("SELECT * FROM `meal`  ORDER BY `id`");
+  rows[0].length === 0 ? res.sendStatus(404) : res.send(rows.slice(0, 1))
+});
+app.get("/first-meal", async (req, res) => {
+  const rows = await knex.raw("SELECT * FROM `meal` ORDER BY `id` LIMIT 1");
+  rows[0].length === 0 ? res.sendStatus(404) : res.send(rows.slice(0, 1))
+});
+app.get("/last-meal", async (req, res) => {
+  const rows = await knex.raw("SELECT * FROM `meal` ORDER BY `id` desc");
+  rows[0].length === 0 ? res.sendStatus(404) : res.send(rows.slice(0, 1))
+});
 
 // for the frontend. Will first be covered in the react class
 app.use("*", (req, res) => {
