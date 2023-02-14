254aae3890bc8a685debc888d18fb0ba72c44787
maccrin <maccrin@gmail.com>
2023-02-14 12:32:23 +0100

    meal router

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -4,12 +4,78 @@ const knex = require("../database");
 
 router.get("/", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meal").select("*");
-    response.json(titles);
-  } catch (error) {
-    throw error;
+    let query = knex('meal')
+    if ("maxPrice" in request.query) {
+      const maxPrice = parseInt(request.query.maxPrice);
+      if (!isNaN(maxPrice)) query = query.where("price", "<", maxPrice)
+      else {
+        return response.status(404).send(`please provide maxprice of the meal`)
+      }
+    }
+    if ("title" in request.query) {
+      const title = request.query.title;
+      query = query.where("title", "like", `%${title}%`)
+
+    }
+
+    if ("dateAfter" in request.query) {
+      const dateAfter = new Date(request.query.dateAfter);
+      if (dateAfter != 'Invalid Date') {
+        query = query.where("when", ">", dateAfter)
+      }
+      else {
+        return response.status(404).send(`please provide valid dateAfter Field in the query`)
+      }
+    }
+
+    if ("dateBefore" in request.query) {
+      const dateBefore = new Date(request.query.dateBefore);
+      if (dateBefore && dateBefore != 'Invalid Date') {
+        console.log(dateBefore)
+        query = query.where("when", "<", dateBefore)
+
+      }
+      else {
+        return response.status(404).send(`please provide valid dateBefore Field in the query`)
+      }
+    }
+    if ("limit" in request.query) {
+      const limit = parseInt(request.query.limit);
+      if (!isNaN(limit)) {
+        query = query.limit(limit)
+      }
+      else {
+        return response.status(404).send(`please provide valid limit value  in the query`)
+      }
+    }
+
+    if ("sortKey" in request.query) {
+      const orderBy = request.query.sortKey.toString().trim();
+      if (orderBy === 'price' || orderBy === 'when' || orderBy === 'max_reservations') {
+        if ("sortDir" in request.query) {
+          let sortOrder = request.query.sortDir.toString().trim().toUpperCase();
+          query = query.orderBy(orderBy, sortOrder)
+        }
+        else {
+          query = query.orderBy(orderBy)
+
+        }
+      }
+      else {
+        response.send(`Invalid sort key`)
+      }
+    }
+    if (request.query.availableReservations === 'true') {
+      query = query.select((knex.raw(` meal.id,meal.title,meal.max_reservations , sum(reservation.number_of_guests)as 'totalReservations'`))).join('reservation', 'reservation.meal_id', '=', 'meal.id').groupBy('meal.id').having('meal.max_reservations', '>', 'totalReservations');
+
+    }
+    const data = await query;
+    data.length ? response.status(200).json(data) : response.send(`No meal Found`);
   }
+  catch (e) {
+    response.status(500).send(e.message)
+  }
+
 });
 
 router.get("/:id", async (request, response) => {
