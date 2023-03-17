1a78989b453567429ee29c2c5fe15589d9a228b3
maccrin <maccrin@gmail.com>
2023-03-17 19:32:36 +0100

    app file

--- src/client/App.js
+++ src/client/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import Meal from './components/meal/meal'
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
-        <p>test</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
+        <Meal />
       </Route>
     </Router>
   );
