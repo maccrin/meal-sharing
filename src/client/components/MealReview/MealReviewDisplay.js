134aa74dc7941c1254a44eab0b5476e877e6f28c
maccrin <maccrin@gmail.com>
2023-04-11 22:14:26 +0200

    display review for each meal

---
+++ src/client/components/MealReview/MealReviewDisplay.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import './mealreview.css';
+const EachMealReviewDisplay = ({ meal }) => {
+    const meal_id = meal.id
+    const [star, setStar] = useState(0);
+    const abortController = new AbortController();
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(`api/meals/${meal_id}/reviews`, {
+                    signal: abortController.signal,
+                });
+                if (res.ok) {
+                    const result = await res.json();
+                    const avgStars = result.reduce((accu, curr) => (accu + curr.stars), 0) / result.length
+                    setStar(avgStars)
+                }
+            } catch (e) {
+                return e.message;
+            }
+        })();
+        return () => {
+            abortController.abort();
+        };
+    }, [meal_id])
+    return (
+        <div>
+            {Array.of(1, 2, 3, 4, 5).map((eachStar) => {
+                return (
+                    <button type="button"
+                        key={eachStar}
+                        className={eachStar <= star ? "on" : "off"}>
+                        <span className="star">&#9733;</span>
+                    </button>)
+            })}
+        </div>
+    )
+
+}
+export default EachMealReviewDisplay;
